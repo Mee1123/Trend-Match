d7ed65cc4c9f886d6b06f1c3b8f50742a0eae799
csuser <csuser@157.105.52.76>
2019-01-12 01:49:59 +0900

    Topページを変更しました。
    ついでにマッチングもつけました.by小野

--- WebContent/WEB-INF/Top.jsp
+++ WebContent/WEB-INF/Top.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page import="java.util.ArrayList"%>
 <!DOCTYPE html >
 <html>
@@ -10,6 +11,16 @@
 <body>
 <jsp:include page="header.jsp"/>
 <h1>アカウントありのトップページ</h1>
-
+	<table>
+		<c:forEach var="user" items="${users}" end = "5">
+			<tr>
+				<div class="form-group">
+					<td><c:out value="${user.getNickname()}" /></td>
+					<td><c:out value="${user.getFreespace()}" /></td>
+				</div>
+			</tr>
+		</c:forEach>
+	</table>
+	<a href = "AccountMatchingResult">もっと見る</a>
 </body>
 </html>
--- src/servlet/Top.java
+++ src/servlet/Top.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import form.SimilarityUserForm;
 import model.User;
 import service.UserService;
 
@@ -20,48 +22,57 @@ import service.UserService;
 public class Top extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Top() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Top() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		HttpSession session = request.getSession();
-		Object userSession =  session.getAttribute("userID");
-		if(userSession == null){
-			System.out.println("Top:セッションが存在しないユーザー");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Login.jsp");
-			dispatcher.forward(request, response);
-		}else {
+		Object userSession = session.getAttribute("userID");
+		if (userSession == null) {
+			System.out.println("Top:[セッションが存在しないユーザー](indexへ飛ばします)");
+			response.sendRedirect("/SE18G2");
+		} else {
 			int userId = (int) userSession;
-			//管理者か
-			if (userId==1) {
+			// 管理者か
+			if (userId == 1) {
 				System.out.println("Top:管理者ユーザー");
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/TopForAdmin.jsp");
 				dispatcher.forward(request, response);
 			}
-			//idサーチ
+			// idサーチ
 			UserService userService = new UserService();
 			User user = userService.accountView(userId);
-			//アカウントなし
+			// アカウントなし
 			if (user.getNickname() == null) {
 				System.out.println("Top:アカウントなしユーザー");
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/TopForNoAccount.jsp");
 				dispatcher.forward(request, response);
-			}//アカウントあり
+			} // アカウントあり
 			else {
+				ArrayList<SimilarityUserForm> users = userService.matching(userId);
+				ArrayList<User> users2 = new ArrayList<>();
+				// user並べ替え
+				users.sort((a, b) -> a.getSimilarityInt() - b.getSimilarityInt());
+				for (SimilarityUserForm similarityuser : users) {
+					users2.add(userService.accountView(similarityuser.getId()));
+				}
 				System.out.println("Top:アカウントありユーザー");
+				request.setAttribute("users", users2);
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Top.jsp");
 				dispatcher.forward(request, response);
 			}
@@ -70,9 +81,11 @@ public class Top extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
