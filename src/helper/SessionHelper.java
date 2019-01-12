2303f989950ed145dc569aa481f8ced98be508a8
csuser <csuser@157.105.52.76>
2019-01-13 01:54:30 +0900

    session切れページ作成
    #130
    issue#130を終わらせました.
    セッション切れページはWebContent/SessionOut.jspにあります.
    サーブレットのメゾットの中身を下のif文で囲うとセッション切れ対策できます.
    if (SessionHelper.sessionCheck(request, response)) {
    ...
    }
    by小野

--- src/helper/SessionHelper.java
+++ src/helper/SessionHelper.java
@@ -1,9 +1,16 @@
 package helper;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 /**
- * セッションに関する関数<br>現状セッション内はuserIDのみ
+ * セッションに関する関数<br>
+ * 現状セッション内はuserIDのみ
+ *
  * @author csuser
  *
  */
@@ -16,5 +23,30 @@ public class SessionHelper {
 	public static void destroyUserSession(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		session.removeAttribute("userID");
+	}
+
+	/**
+	 * セッションがあるか調べます.なければFalse.あればtrueを返します.
+	 *
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public static boolean sessionCheck(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		{
+			HttpSession session = request.getSession();
+			if (session.getAttribute("userID") == null) {
+				System.out.println("セッションが切れてます.");
+
+				request.setCharacterEncoding("UTF-8");
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().append("Served at: ").append(request.getContextPath());
+				response.sendRedirect("/SE18G2/SessionOut.jsp");
+				return false;
+			} else {
+				return true;
+			}
+
+		}
 	}
 }
